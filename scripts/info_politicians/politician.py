58ea220d2c3286d9707123cf8b299ccdbe7ea4a4
harsh1399 <harsh321mahajan@gmail.com>
2024-01-18 14:33:08 -0700

    politician data

---
+++ scripts/info_politicians/politician.py
@@ -0,0 +1,75 @@
+import re
+from functions import *
+redundant = ['image','upright','fullname','short name','founded','nickname', 'alt', 'caption', 'website','image_size','url','title','birth_date','birth_place',
+             'nationality','parents','relatives','residence','occupation','profession','signature','president','president2']
+
+interesting_keys = ['name','office3','term_star3','predecessor3','successor3','office2','term_start2','term_end2','successor2','alma_mater'
+                    'predecessor2','office1','term_start1','term_end1','predecessor1','office','term_start','term_end','predecessor','successor','deputy','office4',
+                    'term_start4','term_end4','predecessor4','successor4','office5','term_start5','term_end5','party','spouse','children']
+
+timeline_keys = ['birth_date', 'medaltemplates', 'years', 'clubs', 'nationalteams']
+
+final_timeline_keys = ['medaltemplates', 'clubs', 'years', 'nationalteams']
+
+def squashMod(json_dict, key):
+    # Nickname and height
+    if key in ['nickname', 'height', 'birth_place', 'weight']:
+        json_dict[key] = re.sub('url(.*)', '', json_dict[key])
+        json_dict[key] = re.sub('https(.*)', '', json_dict[key])
+        json_dict[key] = re.sub('title(.*)', '', json_dict[key])
+
+    # Name and fullname
+        if key in ['name', 'full_name']:
+            json_dict[key] = re.sub("post-nominals","",json_dict[key])
+
+def clubAddendum(json_dict):
+    club_lst = []    
+    for i in range(1,18):
+        if f'years{i}' in json_dict.keys():
+            club_lst.append(i)
+
+    lst = {}
+    for num in club_lst:
+        try:
+            lst[json_dict[f'team{num}']] = json_dict[f'years{num}']
+        except:
+            pass
+                
+        try:
+            del json_dict[f'team{num}']
+        except:
+            pass
+        
+        try:
+            del json_dict[f'years{num}']
+        except:
+            pass
+        
+    if lst:
+        json_dict['clubs'] = lst
+
+def ntAddendum(json_dict):
+    nt_lst = []    
+    for i in range(1,18):
+        if f'nationalyears{i}' in json_dict.keys():
+            nt_lst.append(i)
+
+    lst = {}
+    for num in nt_lst:
+        try:
+            lst[json_dict[f'nationalteam{num}']] = json_dict[f'nationalyears{num}']
+        except:
+            pass
+                
+        try:
+            del json_dict[f'nationalteam{num}']
+        except:
+            pass
+        
+        try:
+            del json_dict[f'nationalyears{num}']
+        except:
+            pass
+        
+    if lst:
+        json_dict['nationalteams'] = lst
